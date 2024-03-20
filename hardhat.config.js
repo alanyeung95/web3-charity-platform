22dfce64b99523e881e0a9d05391d772b3a8a881
alanyeung95 <45751387+alanyeung95@users.noreply.github.com>
2024-03-20 11:44:59 -0400

    chore: Update hardhat config format

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,34 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-
 require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.24",
-  //paths: {
-  //  artifacts: "./src/artifacts",
-  //},
+  solidity: "0.8.20",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
   networks: {
-    hardhat: {
+    /*hardhat: {
       chainId: 1337,
     },
-    //sepolia: {
-    //  url: "https://eth-sepolia.g.alchemy.com/v2/rQ7-cMF4HxDoKZsoyQ_XPrg3kHP5sfYA",
-    //  accounts: [`0x${AVALANCHE_TEST_PRIVATE_KEY}`],
-    //},
+    */
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/{YOUR_API_KEY}",
+      accounts: [`0x{YOUR_METAMASK_PRIVATE_KEY}`],
+    },
   },
 };
