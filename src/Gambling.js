06d3dca32e12e2f80e420826155769a797cb150f
alanyeung95 <45751387+alanyeung95@users.noreply.github.com>
2024-03-27 11:21:26 -0400

    refactor: Auto fetching BTC price, change display unit

--- src/Gambling.js
+++ src/Gambling.js
@@ -10,9 +10,8 @@ const gamblingAddress = process.env.REACT_APP_GAMBLING_ADDRESS;
 const greeterAddress = process.env.REACT_APP_GREETER_ADDRESS;
 
 const GamblingComponent = () => {
-
   const [moneyPool, setMoneyPool] = useState(0);
-  const [depositAmount, setDepositAmount] = useState('');
+  const [depositAmount, setDepositAmount] = useState("");
   // const [withdrawAmount, setWithdrawAmmount] = useState('');
   const [userBalance, setUserBalance] = useState(0);
   const [selectedPre, setSelectedPre] = useState(0);
@@ -31,6 +30,7 @@ const GamblingComponent = () => {
 
   useEffect(() => {
     handleShowInfo();
+    fetchBTCPrice();
   }, []);
 
   // useEffect(() => {
@@ -47,7 +47,6 @@ const GamblingComponent = () => {
   //     contract.removeAllListeners("FundsTransferredToUser");
   //   };
   // }, []);
-
 
   // show money_pool
   async function handleShowInfo() {
@@ -70,14 +69,20 @@ const GamblingComponent = () => {
       if (selectedPre !== 0) {
         if (typeof window.ethereum !== "undefined") {
           await requestAccount();
-          const provider = new ethers.providers.Web3Provider(window.ethereum)
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
           const signer = provider.getSigner();
-          const contract = new ethers.Contract(gamblingAddress, Gambling.abi, signer);
+          const contract = new ethers.Contract(
+            gamblingAddress,
+            Gambling.abi,
+            signer
+          );
 
           const ethValue = ethers.utils.parseEther(depositAmount);
-          const depositTx = await contract.depositAndPredict(selectedPre, { value: ethValue });
+          const depositTx = await contract.depositAndPredict(selectedPre, {
+            value: ethValue,
+          });
           await depositTx.wait();
-          setDepositAmount("")
+          setDepositAmount("");
           handleShowInfo();
         }
       } else {
@@ -88,14 +93,17 @@ const GamblingComponent = () => {
     }
   }
 
-
   async function handlResult() {
     try {
       if (typeof window.ethereum !== "undefined") {
         await requestAccount();
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
-        const contract = new ethers.Contract(gamblingAddress, Gambling.abi, signer);
+        const contract = new ethers.Contract(
+          gamblingAddress,
+          Gambling.abi,
+          signer
+        );
 
         const result = await contract.revealResult();
         await result.wait();
@@ -106,7 +114,7 @@ const GamblingComponent = () => {
     } catch (error) {
       console.error(error);
     }
-  };
+  }
 
   // Fetches the current value store in greeting
   async function fetchBTCPrice() {
@@ -120,7 +128,8 @@ const GamblingComponent = () => {
       );
       try {
         const data2 = await contract.getLatestPrice();
-        setBPrice(ethers.utils.formatEther(data2));
+        const decimalValue = ethers.BigNumber.from(data2).toString();
+        setBPrice(decimalValue);
         // const data3 = await contract.getETHLatestPrice();
         // setEPrice(ethers.utils.formatEther(data3));
       } catch (error) {
@@ -134,12 +143,11 @@ const GamblingComponent = () => {
       <div className="heading">
         <h1>Gambling</h1>
         {/* money_pool */}
-        <h2 >Money pool: {moneyPool}</h2>
+        <h2>Money pool: {moneyPool}</h2>
 
         {/* show prices */}
         <div>
-          <button onClick={fetchBTCPrice}>show price</button>
-          <h3>bitcoin price: {bPrice}</h3>
+          <h3>Bitcoin Price: {bPrice}</h3>
           {/* <h3>Ethereum price: {ePrice}</h3> */}
         </div>
       </div>
@@ -148,9 +156,15 @@ const GamblingComponent = () => {
         <div className="left">
           <h3>Please choose your prediction:</h3>
           <div className="selections">
-            <button className="but" onClick={() => setSelectedPre(1)}>lower</button>
-            <button className="but" onClick={() => setSelectedPre(2)}>unchanged</button>
-            <button className="but" onClick={() => setSelectedPre(3)}>higher</button>
+            <button className="but" onClick={() => setSelectedPre(1)}>
+              lower
+            </button>
+            <button className="but" onClick={() => setSelectedPre(2)}>
+              unchanged
+            </button>
+            <button className="but" onClick={() => setSelectedPre(3)}>
+              higher
+            </button>
           </div>
         </div>
 
@@ -165,24 +179,35 @@ const GamblingComponent = () => {
               placeholder="deposit amount"
               type="number"
             />
-            <button className="but" onClick={handleDeposit} style={{ backgroundColor: "#A1C398" }}>
+            <button
+              className="but"
+              onClick={handleDeposit}
+              style={{ backgroundColor: "#A1C398" }}
+            >
               deposit
             </button>
 
-            <button className="but" onClick={handlResult} style={{ backgroundColor: "#FA7070" }}>show result</button>
+            <button
+              className="but"
+              onClick={handlResult}
+              style={{ backgroundColor: "#FA7070" }}
+            >
+              show result
+            </button>
           </div>
-
         </div>
-
       </div>
-      {selectedPre !== 0 && <h4 style={{ textAlign: "center" }}>You predict that bitcoin price will get
-        <span style={{ fontSize: "20px" }}>
-          {selectedPre === 1 && " Lower "}
-          {selectedPre === 2 && " Unchanged "}
-          {selectedPre === 3 && " Higher "}
-        </span>
-        10 minutes later</h4>}
-
+      {selectedPre !== 0 && (
+        <h4 style={{ textAlign: "center" }}>
+          You predict that bitcoin price will get
+          <span style={{ fontSize: "20px" }}>
+            {selectedPre === 1 && " Lower "}
+            {selectedPre === 2 && " Unchanged "}
+            {selectedPre === 3 && " Higher "}
+          </span>
+          10 minutes later
+        </h4>
+      )}
     </div>
   );
 };
@@ -219,8 +244,11 @@ export default GamblingComponent;
 //   }
 // }
 
-{/* withdraw  */ }
-{/* <button onClick={handleWithdraw} style={{ backgroundColor: "red" }}>
+{
+  /* withdraw  */
+}
+{
+  /* <button onClick={handleWithdraw} style={{ backgroundColor: "red" }}>
         withdraw
       </button>
       <input
@@ -228,4 +256,5 @@ export default GamblingComponent;
         value={withdrawAmount}
         placeholder="withdraw amount"
         type="number"
-      /> */}
+      /> */
+}
